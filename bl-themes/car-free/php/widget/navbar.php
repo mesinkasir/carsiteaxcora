c628308968f41ba0f517f3f63bb4324ec5e0cd41
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2023-06-06 16:58:58 +0700

    Update navbar.php

--- bl-themes/car-free/php/widget/navbar.php
+++ bl-themes/car-free/php/widget/navbar.php
@@ -1,17 +1,17 @@
-<header role="banner bg-dark">
+<header role="banner">
       <nav role="navigation">
         <p><a href="<?php echo Theme::siteUrl(); ?>" title="<?php echo $site->title(); ?>">
-		<img src="<?php echo $site->logo(); ?>" alt="<?php echo $site->title(); ?>" width="120" height="60"/>
+		<img src="<?php echo $site->logo(); ?>" alt="<?php echo $site->title(); ?>" width="120" height="80"/>
 		<?php echo $site->title(); ?></a></p>
-        <ul>
+        <ul style="font-size: 16px;">
           <li>
-		  <a href="<?php echo Theme::siteUrl(); ?>">Home</a>
+	    <a href="<?php echo Theme::siteUrl(); ?>">Home</a>
           </li>
           <?php foreach ($staticContent as $staticPage): ?>
           <li>
-		<a href="<?php echo $staticPage->permalink(); ?>"><?php echo $staticPage->title(); ?></a>
-		</li>
+	   <a href="<?php echo $staticPage->permalink(); ?>"><?php echo $staticPage->title(); ?></a>
+	  </li>
         <?php endforeach ?>
-		</ul>
+	</ul>
       </nav>
 </header>
